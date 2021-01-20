e99e6453cfffaf0f781dabd7893d06b5d1c4e236
王艳 <251140742@qq.com>
2021-01-20 15:25:08 +0800

    Add files via upload

---
+++ src/components/FooterGuide.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello" route>
+    <van-tabbar v-model="active" active-color='#4AA478'>
+      <van-tabbar-item icon="home-o" to='/'>外卖</van-tabbar-item>
+      <van-tabbar-item icon="search" to='/search'>搜索</van-tabbar-item>
+      <van-tabbar-item icon="bill-o" to='/order'>订单</van-tabbar-item>
+      <van-tabbar-item icon="contact" to='/profile'>我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterGuide',
+  data () {
+     return {
+      active: 0,
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/MSite.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        外卖首页
+    </div>
+</template>
+<script>
+export default {
+    name:'MSite'
+}
+</script>
